a7fed2ccf4ea932a0020c3c7c6eb8b68a4aad3fb
Stuart Read <stuart.read@lightsource.ca>
2018-07-11 10:01:37 -0600

    committing some untracked "test" scripts

---
+++ test_params.py
@@ -0,0 +1,20 @@
+import struct
+
+from agilent import _get_params
+
+filename = "var/2017-11-10 4X-25X/2017-11-10 4x-25x.dmt"
+#filename = "var/2017-11-10 4X-25X/2017-11-10 4x-25x.bsp"
+#filename = "var/2018-01-02 Small Test File/4_noimage_agg256.bsp"
+#filename = "var/2018-01-02 Small Test File/background_agg256.bsp"
+#filename = "var/2018-01-02 Small Test File/background_agg1024.bsp"
+filename = "C:\\Users\\reads\\tmp\\aff-testdata\\2017-11-24 USAF after FPA pumpdown\\USAF 25X Mosaic\\usaf 25x mosaic.bsp"
+
+with open(filename, 'rb') as f:
+    d = _get_params(f)
+
+for k, v in d.items():
+    try:
+        for k2, v2 in v.items():
+            print("{0} : {1}".format(k2, v2))
+    except AttributeError:
+        print("{0} : {1}".format(k, v))
---
+++ test_tile_order.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+from agilent import agilentImage, agilentMosaic
+
+filename = "/data/users/Kelly/557-S2_1hrTFP_PT/557-S2_1hrTFP_PT.dms"
+MAT = False
+
+am = agilentMosaic(filename, MAT=MAT)
+print(am.data.shape)
+img = am.data.sum(axis=2)
+print(img.shape)
+if MAT:
+    imgplot = plt.imshow(img, origin='upper')
+else:
+    imgplot = plt.imshow(img, origin='lower')
+plt.show()
